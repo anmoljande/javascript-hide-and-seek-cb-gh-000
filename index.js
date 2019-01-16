091d8525fba56305d2746b10889d2cb4483b651f
Anmol Jande <jandanmol@gmail.com>
2019-01-16 12:40:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+function getFirstSelector(selector)
+{
+  console.log("Hiiii"+selector);
+  var ele=document.querySelector(selector);
+  return ele;
+}
+function nestedTarget()
+{
+ 
+ return document.querySelector('#nested .target ') 
 
+  
+}
+
+function increaseRankBy(n)
+{
+  var ele=document.querySelectorAll('ul.ranked-list li');
+  for(let i=0;i<ele.length;i++)
+  {
+    var a=parseInt(ele[i].innerHTML,10);
+    a=a+n;
+    ele[i].innerHTML=a;
+  }
+  
+}
+
+function deepestChild()
+{
+  
+  
+}
