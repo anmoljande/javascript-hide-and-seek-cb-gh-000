34049da777f55e1d67e9e05484e69eace01fbd9e
Anmol Jande <jandanmol@gmail.com>
2019-01-16 12:57:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
+function getFirstSelector(selector)
+{
+  console.log("Hiiii"+selector);
+  var ele=document.querySelector(selector);
+  return ele;
+}
+function nestedTarget()
+{
+ 
+ return document.querySelector('#nested .target ') 
 
+  
+}
+
+function increaseRankBy(n)
+{
+  var ele=document.querySelectorAll('ul.ranked-list li');
+  for(let i=0;i<ele.length;i++)
+  {
+    var a=parseInt(ele[i].innerHTML,10);
+    a=a+n;
+    ele[i].innerHTML=a;
+  }
+  
+}
+
+function deepestChild()
+{
+  var curr=document.getElementById('grand-node');
+  var next=curr.children[0];
+  while(next)
+  {
+    curr=next;
+    next=curr.children[0];
+    
+  }
+  return curr;
+}
